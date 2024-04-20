cabd26f5831113bb5a9cca20c3c8c3b2f00563a2
Maxi-Mega <ixam.agem@gmail.com>
2024-04-20 17:45:09 +0200

    chore: reformat code

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,9 +5,9 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { DefaultApolloClient } from "@vue/apollo-composable";
 
+import { apolloClient } from "@/apollo";
 import App from "./App.vue";
 import router from "./router";
-import { apolloClient } from "@/apollo";
 
 const app = createApp(App);
 
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
+import { type IStaticMethods } from "preline/preline";
+
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods;
+  }
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +17,14 @@ const router = createRouter({
       component: HomeView,
     },
   ],
+});
+
+router.afterEach((to, from, failure) => {
+  if (!failure) {
+    setTimeout(() => {
+      window.HSStaticMethods.autoInit();
+    }, 100);
+  }
 });
 
 export default router;
