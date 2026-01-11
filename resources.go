502eb46030e4e4073d3f69d921a7030476ddc590
Maxi-Mega <ixam.agem@gmail.com>
2026-01-11 12:04:24 +0100

    feat: improve doc, use Go template

--- resources.go
+++ resources.go
@@ -5,13 +5,14 @@ import (
 	"embed"
 	"fmt"
 	"io"
+	"path/filepath"
 )
 
 //go:embed resources/sample-config.yml
 var sampleConfig []byte
 
-//go:embed resources/expr-doc/expr.md
-var exprDoc []byte
+//go:embed resources/*_doc.md
+var docs embed.FS
 
 //go:embed all:frontend/dist
 var frontend embed.FS
@@ -22,8 +23,32 @@ func printSampleConfig(w io.Writer) error {
 }
 
 func printDocumentation(w io.Writer) error {
-	docStart := bytes.IndexByte(exprDoc, '\n') + 2 // skip the first two lines (comment)
-	_, err := fmt.Fprintln(w, string(exprDoc[docStart:]))
+	const docDir = "resources"
 
-	return err //nolint:wrapcheck
+	docFiles, err := docs.ReadDir(docDir)
+	if err != nil {
+		return fmt.Errorf("bad documentation dir: %w", err)
+	}
+
+	for _, docFile := range docFiles {
+		if docFile.IsDir() {
+			continue
+		}
+
+		doc, err := docs.ReadFile(filepath.Join(docDir, docFile.Name()))
+		if err != nil {
+			return fmt.Errorf("can't read doc file: %w", err)
+		}
+
+		var docStart int
+
+		if bytes.HasPrefix(doc, []byte("[//]: # Code generated")) {
+			docStart = bytes.IndexByte(doc, '\n') + 2 // skip the first two lines (comment)
+		}
+
+		_, _ = fmt.Fprintln(w, string(doc[docStart:]))
+		_, _ = fmt.Fprintln(w)
+	}
+
+	return nil
 }
