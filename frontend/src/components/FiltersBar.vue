e4668d94c983335e83ccfb842ff316b4a6de558d
Maxi-Mega <ixam.agem@gmail.com>
2026-06-21 09:59:40 +0200

    feat: add text filter clear button

--- frontend/src/components/FiltersBar.vue
+++ frontend/src/components/FiltersBar.vue
@@ -3,24 +3,48 @@ import RangeInput from "@/components/RangeInput.vue";
 import { useFilterStore } from "@/stores/filters";
 import { useImageStore } from "@/stores/images";
 import { useStaticInfoStore } from "@/stores/static_info";
+import { X } from "@lucide/vue";
+import { useTemplateRef } from "vue";
 
 const staticInfo = useStaticInfoStore();
 const imageStore = useImageStore();
 const filterStore = useFilterStore();
+
+const textSearchInput = useTemplateRef<HTMLInputElement>("filters-text-search");
+
+function clearTextSearchInput() {
+  if (textSearchInput.value) {
+    (document.getElementById(textSearchInput.value.id) as HTMLInputElement).value = "";
+    filterStore.searchQuery = "";
+    textSearchInput.value.focus();
+  }
+}
 </script>
 
 <template>
   <div
-    class="mt-5 items-center gap-1 overflow-x-auto pb-2 sm:mt-0 sm:justify-end sm:overflow-x-visible sm:ps-5 sm:pb-0"
+    class="mt-5 grow-[0.2] items-center gap-1 overflow-x-auto pb-2 sm:mt-0 sm:justify-end sm:overflow-x-visible sm:ps-5 sm:pb-0"
     style="display: inherit"
   >
-    <div class="min-w-12 space-y-3 px-1">
-      <input
-        type="search"
-        class="block w-full rounded-md border border-gray-100 bg-transparent px-2 py-1 text-base text-gray-200 placeholder-gray-300 placeholder-shown:border-neutral-200 focus:border-blue-500 focus:ring-neutral-600"
-        placeholder="Search"
-        @input="(e) => (filterStore.searchQuery = (e.target as HTMLInputElement).value)"
-      />
+    <div class="min-w-12 grow space-y-3 px-1">
+      <div class="relative">
+        <input
+          id="filters-text-search"
+          ref="filters-text-search"
+          type="search"
+          class="block w-full rounded-md border border-gray-100 bg-transparent px-2 py-1 text-base text-gray-200 placeholder-gray-300 placeholder-shown:border-neutral-200 focus:border-blue-500 focus:ring-neutral-600"
+          placeholder="Search"
+          @input="(e) => (filterStore.searchQuery = (e.target as HTMLInputElement).value)"
+        />
+        <button
+          type="button"
+          title="Clear input"
+          class="text-muted-foreground focus:text-primary-focus absolute inset-y-0 inset-e-0 z-20 flex cursor-pointer items-center rounded-e-md px-1 focus:outline-hidden"
+          @click="clearTextSearchInput"
+        >
+          <X :size="16" color="var(--color-gray-100)" />
+        </button>
+      </div>
     </div>
     <p
       v-if="staticInfo.staticInfo.maxImagesDisplayCount > imageStore.totalCount"
